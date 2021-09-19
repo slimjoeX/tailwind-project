b9ed47a3e1d66c6c8b5478e61ccdd471bc881105
Steve <retroslimjoe@hotmail.com>
2021-09-19 10:37:11 -0400

    Move hamburger button to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        '250': '250ms'
+      }
+    },    
   },
   variants: {
     extend: {},
